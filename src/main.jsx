b6b783ab9efdb3e856b6e5a39c6ac9ba80b041de
RutujaPatil-01 <patilrutuja737@gmail.com>
2025-09-28 15:41:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AppContextProvider from './Context/AppContext.jsx'
+import { Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   
+   <Router>
+    <AppContextProvider>
     <App />
-   
-    
+    </AppContextProvider>
+   </Router>
+ 
   </StrictMode>,
 )
